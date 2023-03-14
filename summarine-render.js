9c30d188ee72abe5524aa29672f41343774322e2
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2023-03-14 16:20:45 +0100

    What is a node?

---
+++ summarine-render.js
@@ -0,0 +1,9 @@
+const summarine = require("./summarine/render");
+
+if (process.argv.length < 4) {
+    throw Error("Not all arguments were supplied");
+}
+
+const coursePath = process.argv[2];
+const filename = process.argv[3];
+
---
+++ summarine/render.js
@@ -0,0 +1,8 @@
+let MarkdownIt = require('markdown-it'),
+md = new MarkdownIt();
+
+function render() {
+
+}
+
+module.exports = { render };
